a0bf6598f4fd6820fd2283fdc2634697aa25dff1
Starlet <gpyron@mail.com>
2018-08-13 18:06:02 -0400

    Implement basic IO.

---
+++ CTREmulator/CTR/IO/CFG9.cs
@@ -0,0 +1,19 @@
+﻿using CTREmulator.ARM;
+
+namespace CTREmulator.CTR.IO
+{
+    partial class IOHandler
+    {
+        private byte CFG9_RST11(Interpreter CPU)
+        {
+            // TODO: Implement this
+
+            Logging.WriteInfo("Called.");
+
+            // 
+            // CPU.ReadUInt8(0x0);
+
+            return 0;
+        }
+    }
+}
