1a9776c568c3f8d53079f23c42d5fcde8c6265b1
Starlet Lockhomes <gpyron@mail.com>
2018-12-19 23:53:25 -0500

    Remove unused variable in logger + add bitwise helper

---
+++ LemonLime.Common/Bitwise.cs
@@ -0,0 +1,9 @@
+﻿namespace LemonLime.Common
+{
+    public class Bitwise
+    {
+        public static bool GetBit(byte   Input, int Position) { return (Input & (1 << Position - 1)) != 0; }
+        public static bool GetBit(ushort Input, int Position) { return (Input & (1 << Position - 1)) != 0; }
+        public static bool GetBit(uint   Input, int Position) { return (Input & (1 << Position - 1)) != 0; }
+    }
+}
